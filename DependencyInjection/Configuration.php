79c6d82f6d9b0f0bcc71a1a170bef1f8afbdc7c0
Pavel Kučera <pave.kucera@gmail.com>
2014-08-17 09:06:19 +0200

    Configuration.

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Copyright (c) 2014 Pavel Kučera (http://github.com/pavelkucera)
+ */
+
+namespace Kucera\MonologExtensionsBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('kucera_monolog_extensions');
+
+        $rootNode->children()
+            ->arrayNode('handlers')
+                ->useAttributeAsKey('name')
+                ->canBeUnset()
+                ->prototype('array')
+                    ->children()
+                        ->scalarNode('log_directory')->defaultValue('%kernel.logs_dir%/blueScreen')->end()
+                        ->scalarNode('level')->defaultValue('DEBUG')->end()
+                        ->booleanNode('bubble')->defaultTrue()->end()
+                    ->end()
+                ->end()
+            ->end()
+        ->end();
+
+        return $treeBuilder;
+    }
+}
